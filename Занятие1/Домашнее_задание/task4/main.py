cf559474df504ade4bd34de50a6749287fa7b452
aleuli <alek-ulitin@yandex.ru>
2021-10-16 13:59:40 +0300

    idl

---
+++ Занятие1/Домашнее_задание/task4/main.py
@@ -0,0 +1,3 @@
+a = list(range(100,0, -4))
+b = sum(a) / len(a)
+print(int(b))
--- Занятие1/Лабораторные задания/task4_1/main.py
+++ Занятие1/Лабораторные задания/task4_1/main.py
@@ -2,3 +2,5 @@ list_1 = [1, 2, 3]
 list_2 = [3, 2, 1]
 
 # TODO сложить два списка
+list_3 = list_1 + list_2
+print(list_3)
