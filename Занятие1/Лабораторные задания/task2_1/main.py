c33457353e9465b7e054794acbc286d0692b91be
aleuli <alek-ulitin@yandex.ru>
2021-10-16 13:07:02 +0300

    idl

--- Занятие1/Лабораторные задания/task2_1/main.py
+++ Занятие1/Лабораторные задания/task2_1/main.py
@@ -1,5 +1,14 @@
 tuple_ = (8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9)
 
-sum(tuple_)
+a = sum(tuple_)
+print(a)
+b = len(tuple_)
+print(b)
+c = a / b
+print(c)
+d = min(tuple_)
+print(d)
+f = max(tuple_)
+print(f)
 
 # TODO Каждое из значений печатать на отдельной строке
