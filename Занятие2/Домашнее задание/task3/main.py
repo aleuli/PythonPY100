5b116f225251e4d68b3d05830f2f6c8e334513f5
aleuli <alek-ulitin@yandex.ru>
2021-10-17 22:25:12 +0300

    idl

--- Занятие2/Домашнее задание/task3/main.py
+++ Занятие2/Домашнее задание/task3/main.py
@@ -1,6 +1,12 @@
 # TODO
-A = 45
-B = 20
-C = 30
-
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+c = int(input("Введите число c: "))
+rezultat1 = a < 45 and 45 <= b and 45 <= c
+rezultat2 = 45 <= a and 45 > b and 45 <= c
+rezultat3 = 45 <=a and 45 <= b and 45 > c
+if rezultat1 or rezultat2 or rezultat3:
+    print("Условие истинно")
+else:
+    print("Условие ложно")
 
--- Занятие2/Лабораторные_задания/task2_1/main.py
+++ Занятие2/Лабораторные_задания/task2_1/main.py
@@ -2,3 +2,4 @@ if __name__ == "__main__":
     count_stairs = 4  # TODO количество ступеней
     for i in range(1, count_stairs + 1):  # TODO как получить последовательность чисел?
         print(i * "*")  # TODO печать ступени
+
