32551b7d337c100e84c4326fe28ab026601c7c74
AleUli <alek-ulitin@yandex.ru>
2021-10-21 19:39:13 +0300

    init

--- Занятие3/Лабораторные_задания/task1_7/main.py
+++ Занятие3/Лабораторные_задания/task1_7/main.py
@@ -1,3 +1,11 @@
+def student(a,b,s):
+    m = 0
+    while 0 < s :
+        s += a
+        b = b + (b * 0.05)
+        s = s - b
+        m += 1
+        print("Я прожил", m,"месяцев")
+    return m
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    print(student(1000,1500,30000))
