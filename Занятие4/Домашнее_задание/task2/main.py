eb8537a68c2b860e0cf89b1fbe33238ef0586f7c
aleuli <771307qqqqwE>
2021-10-28 13:33:47 +0300

    int

--- Занятие4/Домашнее_задание/task2/main.py
+++ Занятие4/Домашнее_задание/task2/main.py
@@ -1,15 +1,13 @@
 def function(integer:int):
-    integers_string = [int(d) for d in str(integer)]
-    for number in integers_string:
-        if set(number) == number:
-            print("Есть одинаковые")
-
-
-
-if __name__ == "__main__":
+    list_number = [int(d) for d in str(integer)]
+    if len(set(list_number)) < len(list_number):
+        print("Не все цифры числа уникальны")
+    else:
+        print("Все цифры числа уникальны")
 
 
 if __name__ == "__main__":
 
-    print(function(44))
+
+    print(function(1234561789))
 
