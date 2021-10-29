0c1edb1a90112d31e98ba3f0ffe19bb2c3dc5c7d
aleuli <771307qqqqwE>
2021-10-29 13:17:31 +0300

    int

--- Занятие5/Домашнее_задание/task1/main.py
+++ Занятие5/Домашнее_задание/task1/main.py
@@ -1,3 +1,15 @@
 if __name__ == "__main__":
     # Write your solution here
     pass
+
+count_row = 10
+count_col = 10
+matrix =[
+    [i * j for j in range(1,count_col)]
+    for i in range(1,count_row)
+]
+
+for row in range(len(matrix)):
+    for col in range(len(matrix[0])):
+        print(f"{matrix[row][col]:2}", end=" ")
+    print()
